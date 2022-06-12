2c8ba43a2052d288bdda424800a3f7ba73372e75
vandeurenglenn <vandeurenglenn@gmail.com>
2022-06-12 16:58:50 +0200

    Simplify iconset & fix setting getter

--- custom-svg-iconset.js
+++ custom-svg-iconset.js
@@ -1,103 +1,115 @@
-var customSvgIconSet = (function () {
-'use strict';
+/**
+ * @extends HTMLElement
+ */
+export default ((base = HTMLElement) => {
+  globalThis.svgIconset = globalThis.svgIconset || {};
 
-var customSvgIconset = ((base = HTMLElement) => {
-  window.svgIconset = window.svgIconset || {};
   customElements.define('custom-svg-iconset', class CustomSvgIconset extends base {
-    static get observedAttributes() {
-      return ['name', 'theme', 'size', 'width', 'height'];
-    }
     constructor() {
       super();
     }
     connectedCallback() {
-      if (!this.hasAttribute('name')) {
-        this.name = this.name;
-      }
+      !this.name && this.setAttribute('name', 'icons')
+      globalThis.svgIconset[this.name] = this;
+      globalThis.dispatchEvent(new CustomEvent('svg-iconset-update'));
+      globalThis.dispatchEvent(new CustomEvent('svg-iconset-added', {detail: this.name}));
+
       this.style.display = 'none';
     }
+    /**
+     * The name of the iconset
+     * @default {string} icons
+     */
     get name() {
-      return this._name || 'icons';
+      return this.getAttribute('name');
     }
-    get theme() {
-      return this._theme || 'light';
-    }
+
+    /**
+     * The width of the viewBox
+     * @default {Number} 24
+     */
     get width() {
-      return this.getAttribute('width') || 24;
+      return this.getAttribute('width') || 24
     }
+
+    /**
+     * The height of the viewBox
+     * @default {Number} 24
+     */
     get height() {
-      return this.getAttribute('height') || 24;
+      return this.getAttribute('height') || 24
     }
-    get size() {
-      return this._size || { width: this.width, height: this.height };
-    }
-    set name(value) {
-      if (this._name !== value) {
-        this._name = value;
-        window.svgIconset[value] = { host: this, theme: this.theme };
-        window.dispatchEvent(new CustomEvent('svg-iconset-update'));
-        window.dispatchEvent(new CustomEvent('svg-iconset-added', { detail: value }));
-      }
-    }
-    set theme(value) {
-      if (this._theme !== value && this.name) {
-        window.svgIconset[this.name] = { host: this, theme: value };
-        window.dispatchEvent(new CustomEvent('svg-iconset-update'));
-      }
-      this._theme = value;
-    }
-    set size(value) {
-      let width;
-      let height;
-      if (!Array.isArray(value)) {
-        width = 24;
-        height = 24;
-      }
-      this._size = { width, height };
-    }
-    attributeChangedCallback(name, oldVal, newVal) {
-      if (oldVal !== newVal) {
-        this[name] = newVal;
-      }
-    }
+
+    /* from https://github.com/PolymerElements/iron-iconset-svg */
+    /**
+     * Applies an icon to given element
+     * @param {HTMLElement} element the element appending the icon to
+     * @param {string} icon The name of the icon to show
+     */
     applyIcon(element, icon) {
       element = element.shadowRoot || element;
       this.removeIcon(element);
-      this._cloneIcon(icon).then(icon => {
+      this.#cloneIcon(icon).then(icon => {
         element.insertBefore(icon, element.childNodes[0]);
-        element._iconSetIcon = icon;
+        element._iconSetIcon = icon
       });
     }
+    /**
+     * Remove an icon from the given element by undoing the changes effected
+     * by `applyIcon`.
+     *
+     * @param {Element} element The element from which the icon is removed.
+     */
     removeIcon(element) {
+      // Remove old svg element
       element = element.shadowRoot || element;
       if (element._iconSetIcon) {
         element.removeChild(element._iconSetIcon);
         element._iconSetIcon = null;
       }
     }
-    _cloneIcon(id) {
+    /**
+     * Produce installable clone of the SVG element matching `id` in this
+     * iconset, or `undefined` if there is no matching element.
+     *
+     * @return {Element} Returns an installable clone of the SVG element
+     * matching `id`.
+     * @private
+     */
+    #cloneIcon(id) {
       return new Promise((resolve, reject) => {
         try {
-          this._icons = this._icons || this._createIconMap();
-          let svgClone = this._prepareSvgClone(this._icons[id], this.size);
+          this._icons = this._icons || this.#createIconMap();
+          let svgClone = this.#prepareSvgClone(this._icons[id]);
           resolve(svgClone);
         } catch (error) {
           reject(error);
         }
       });
     }
-    _createIconMap() {
+    // TODO: Update icon-map on child changes
+    /**
+     * Create a map of child SVG elements by id.
+     *
+     * @return {!Object} Map of id's to SVG elements.
+     * @private
+     */
+    #createIconMap() {
       var icons = Object.create(null);
-      this.querySelectorAll('[id]').forEach(icon => {
-        icons[icon.id] = icon;
-      });
+      this.querySelectorAll('[id]')
+        .forEach(icon => {
+          icons[icon.id] = icon;
+        });
       return icons;
     }
-    _prepareSvgClone(sourceSvg, { width, height }) {
+    /**
+     * @private
+     */
+    #prepareSvgClone(sourceSvg) {
       if (sourceSvg) {
         var content = sourceSvg.cloneNode(true),
             svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg'),
-            viewBox = content.getAttribute('viewBox') || '0 0 ' + width + ' ' + height,
+            viewBox = content.getAttribute('viewBox') || '0 0 ' + this.width + ' ' + this.height,
             cssText = 'pointer-events: none; display: block; width: 100%; height: 100%;';
         svg.setAttribute('viewBox', viewBox);
         svg.setAttribute('preserveAspectRatio', 'xMidYMid meet');
@@ -107,10 +119,5 @@ var customSvgIconset = ((base = HTMLElement) => {
       }
       return null;
     }
-  });
+  })
 })();
-
-return customSvgIconset;
-
-}());
-//# sourceMappingURL=custom-svg-iconset.js.map
